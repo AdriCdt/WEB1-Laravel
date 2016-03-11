fbcdf67de0f19331af6bbb362c07d21c77d83486
Adri Cdt <adrien.cadot@hotmail.fr>
2016-03-11 12:27:07 +0100

    first commit

---
+++ resources/views/articles/show.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Article n°{{ $post->id }}</h1>
+    <h2>{{$post->title}} <br> Auteur: {{ $post->user->name }} </h2>
+    <p>{{$post->content}}</p>
+
+@endsection
